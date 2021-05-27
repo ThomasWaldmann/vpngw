25aea5f90e9569132ec1bf5c17216eb4289b07c7
Thomas Waldmann <tw@waldmann-edv.de>
2021-05-27 21:10:35 +0200

    update openvpn scripts

---
+++ gw/etc/openvpn/scripts/vpn-routing.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# called by the systemd vpn-routing service ONCE
+
+# if marked 1, use standard, non-VPN routing
+ip rule add fwmark 1 table main
+
+# if marked 2/3/4, use the VPN routing tables
+ip rule add fwmark 2 table t_tun0
+ip rule add fwmark 3 table t_tun1
+ip rule add fwmark 4 table t_tun2
+
+# set a non-working default route for the VPNs
+ip route add default dev lo table t_tun0
+ip route add default dev lo table t_tun1
+ip route add default dev lo table t_tun2
+
+# testing this:
+# ip route get 8.8.8.8 mark 1   -> should show non-vpn route
+# ip route get 8.8.8.8 mark 2   -> should show tun0 vpn route
+
+
